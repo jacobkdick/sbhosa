6a85ffcb6d262322c04a8c156491cbaa9b6a8e7b
jacobkdick <jacobkdick@gmail.com>
2016-11-25 02:48:03 +0000

    Added error reporting for login state

--- pg/views/login/login.controller.js
+++ pg/views/login/login.controller.js
@@ -8,4 +8,20 @@ angular
   $scope.siteNavLinks = NAV_LINKS.internal;
   
   $scope.user = Auth.$getAuth();
+  $scope.loginUser = {
+    email: null,
+    password: null,
+  };
+  
+  $scope.signIn = function () {
+    $scope.error = null;
+    
+    Auth.$signInWithEmailAndPassword($scope.loginUser.email, $scope.loginUser.password)
+    .then(function (firebaseUser) {
+      $state.go('admin');
+    }).catch(function (err) {
+      $scope.error = 'Error signing in.';
+      console.error('Auth failed: ', err);
+    });
+  };
 }]);
